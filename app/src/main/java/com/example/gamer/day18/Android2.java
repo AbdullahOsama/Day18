ead327901dd59eb3f3e6b7b9b61f5eb63e42df3d
Gamer <zeerak980@gmail.com>
2017-08-23 08:34:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gamer/day18/Android2.java
@@ -0,0 +1,29 @@
+package com.example.gamer.day18;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Android2 extends Fragment {
+
+
+    public Android2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_android3, container, false);
+    }
+
+}
